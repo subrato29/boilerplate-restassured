890b5a57ecc40c092fcddea26037432674454c6e
subrato.sarkar <subrato.sarkar@lookout.com>
2022-11-11 19:18:05 -0500

    updated code

--- src/test/java/com/api/lib/ApiUtils.java
+++ src/test/java/com/api/lib/ApiUtils.java
@@ -13,39 +13,11 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class ApiUtils extends HttpUtils {
+
 	/**
-	 * Keyword: validateReponsePayload
-	 * Author: Subrato Sarkar
-	 * Date: 11/10/2020
-	 * @return responseCode
-	 */
-	public static void validateResponsePayload (String requestPayLoad, String URI) {
-		JsonPath jsonPath = get(URI).jsonPath();
-		String[] reqPayLoad = requestPayLoad.split(",");
-		String key = null, value = null;
-		int count = 0;
-		Map<String, String> map = new HashMap<String, String>();
-		for (int i = 0; i < reqPayLoad.length; i++) {
-			key = reqPayLoad[i].split(":")[0].trim();
-			value = reqPayLoad[i].split(":")[1].trim();
-			if (String.valueOf(jsonPath.get(key.toLowerCase())).equals(value)) {
-				count ++;
-			} else {
-				map.put(key, value);
-			}
-		}
-		if (count == reqPayLoad.length) {
-			ReportUtil.markPassed("Response payload values is verified successfully with request payload");
-		} else {
-			ReportUtil.markFailed("Unverified: " + map.toString());
-		}
-	}
-	
-	/**
-	 * Keyword: validateReponseHeader
-	 * Author: Subrato Sarkar
-	 * Date: 11/10/2020
-	 * @return responseCode
+	 *
+	 * @param URI
+	 * @param response
 	 */
 	public static void validateResponseHeader (String URI, Response response) {
 		if (response.header("content-type").contains("application/json")) {
@@ -54,7 +26,12 @@ public class ApiUtils extends HttpUtils {
 			ReportUtil.markFailed("Reponse header is not validated");
 		}
 	}
-	
+
+	/**
+	 *
+	 * @param response
+	 * @return
+	 */
 	public static long getResponseTime (Response response) {
 		return response.getTime();
 	}
@@ -103,26 +80,6 @@ public class ApiUtils extends HttpUtils {
 
 	/**
 	 *
-	 * @param request_body
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public static JSONObject parse_json_to_delete_tracks_from_playlist (String request_body) {
-		List<JSONObject> list = new ArrayList<JSONObject>();
-		JSONObject json = new JSONObject();
-		String[] array = request_body.split(",");
-		for (int i = 0; i < array.length; i++) {
-			json.put("uri", "spotify:track:" + array[i].trim());
-			list.add(json);
-			json = new JSONObject();
-		}
-		json = new JSONObject();
-		json.put("tracks", list);
-		return json;
-	}
-
-	/**
-	 *
 	 * @param trackId
 	 * @return
 	 */
@@ -134,5 +91,4 @@ public class ApiUtils extends HttpUtils {
 		}
 		return list.toArray(new String[list.size()]);
 	}
-	
 }
