625f24e7817e9471aaac606c277d99e2e487c959
subrato.sarkar <subrato.sarkar@lookout.com>
2022-11-23 17:21:30 -0500

    updated JsonUtils

--- src/main/java/com/api/utilities/JsonUtils.java
+++ src/main/java/com/api/utilities/JsonUtils.java
@@ -5,9 +5,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class JsonUtils {
 
@@ -17,21 +14,16 @@ public class JsonUtils {
      * @param jsonObjectKey
      * @return
      */
-    public static List<String> getJsonBody (String nameOfJsonFile, String jsonObjectKey) {
+    public static JSONArray getJsonArray (String nameOfJsonFile, String jsonObjectKey) {
         final String JSON_DATA_PATH = Constants.FRAMEWORK_ROOT_DIRECTORY + "/src/main/resources/jsonData/";
-        List<String> list = new ArrayList<>();
         JSONParser parser = new JSONParser();
         try {
             Object obj = parser.parse(new FileReader(JSON_DATA_PATH + nameOfJsonFile + ".json"));
             JSONObject jsonObject = (JSONObject) obj;
-            JSONArray jsonArray = (JSONArray) jsonObject.get(jsonObjectKey);
-            Iterator<JSONObject> iterator = jsonArray.iterator();
-            while (iterator.hasNext()) {
-                list.add (String.valueOf(iterator.next()));
-            }
+            return (JSONArray) jsonObject.get(jsonObjectKey);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
+        return null;
     }
 }
