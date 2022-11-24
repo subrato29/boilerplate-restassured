33f72f4f94aa25f542aac0f45b1b100c12f54c9a
subrato.sarkar <subrato.sarkar@lookout.com>
2022-11-23 19:41:58 -0500

    updated JsonUtils

--- src/main/java/com/api/utilities/JsonUtils.java
+++ src/main/java/com/api/utilities/JsonUtils.java
@@ -8,21 +8,37 @@ import java.io.FileReader;
 
 public class JsonUtils {
 
+    static final String JSON_DATA_PATH = Constants.FRAMEWORK_ROOT_DIRECTORY + Constants.JSON_DATA_PATH;
+
     /**
      * Based on jsonKey, json array object will be returned
-     * @param nameOfJsonFile
+     * @param jsonFileName
      * @param jsonObjectKey
      * @return
      */
-    public static JSONArray getJsonArray (String nameOfJsonFile, String jsonObjectKey) {
-        final String JSON_DATA_PATH = Constants.FRAMEWORK_ROOT_DIRECTORY + Constants.JSON_DATA_PATH;
-        JSONParser parser = new JSONParser();
+    public static JSONArray getJsonArrayByKey (String jsonFileName, String jsonObjectKey) {
+        JSONParser jsonParser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader(JSON_DATA_PATH + nameOfJsonFile + ".json"));
+            Object obj = jsonParser.parse(new FileReader(JSON_DATA_PATH + jsonFileName + ".json"));
             JSONObject jsonObject = (JSONObject) obj;
             return (JSONArray) jsonObject.get(jsonObjectKey);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Read the whole JSON body
+     * @param jsonFileName
+     * @return
+     */
+    public static JSONArray getJsonBody (String jsonFileName) {
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(JSON_DATA_PATH + jsonFileName + ".json")) {
+            return (JSONArray)jsonParser.parse(reader);
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
         return null;
     }
