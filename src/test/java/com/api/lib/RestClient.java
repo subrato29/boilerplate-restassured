e5bd9b9fab084b559839503add59631970288de6
subrato.sarkar <subrato.sarkar@lookout.com>
2022-12-01 18:42:02 -0500

    removing irrelevent imports

--- src/test/java/com/api/lib/RestClient.java
+++ src/test/java/com/api/lib/RestClient.java
@@ -1,8 +1,6 @@
 package com.api.lib;
 
-import static io.restassured.RestAssured.given;
 import org.json.simple.JSONObject;
-import com.api.reports.ReportUtil;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
